0a9976c4456a88c56e6a695095be9cbaca975ef6
jonathan <jonathanibrahim22@yahoo.com>
2020-05-02 19:26:44 +0700

    add default avatar untuk foto, tidak ada delay untuk load on complete, add jquery unobtrusive reference

--- RecycleHelperApplication/Content/loader/loader.js
+++ RecycleHelperApplication/Content/loader/loader.js
@@ -1,9 +1,10 @@
 ﻿function startLoaderScreen()
 {
+    $("#loaderScreen").empty();
     $("#loaderScreen").addClass("preloader-wrapper");
     document.getElementById('loaderScreen').insertAdjacentHTML('beforeend', '<div class="outer-container"><div class="inner-container"><div class="centered-content"><div class="lds-ring"><div></div><div></div><div></div><div></div></div><div class="centered-content"><span style="font-size: 120%;font-weight: bold;">Loading...</span></div></div></div></div>');
     document.getElementById("loaderScreen").style.display = "block";
 }
 function stopLoaderScreen() {
-    setTimeout(function () { $('#loaderScreen').delay(1000).fadeOut(); }, 1500);
+    $('#loaderScreen').fadeOut();
 }
